33c337af6dcd19036db042fb89cb9e5af595f642
Jamie Richison <jamierichi@yahoo.com>
2019-12-02 16:22:24 -0600

    base goals complete

---
+++ src/components/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class About extends React.Component{
+    render(){
+        return(
+            <>
+            <h2>About</h2>
+            <p>This is a paragraph about some stuff</p>
+            </>
+        )
+    }
+}
+export default About;
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Contact extends React.Component{
+    render(){
+        return(
+            <>
+            <h2>Contact</h2>
+            <p>Phone: 1-800-WE-REACT</p>
+            <p>email: info@ReactActors.com</p>
+            </>
+        )
+    }
+}
+export default Contact;
